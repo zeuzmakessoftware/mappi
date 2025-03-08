b326920011b27b8a021da5300bc4c6e9268661b3
zeuzmakessoftware <aodude@icloud.com>
2025-03-08 11:29:14 -0800

    map

---
+++ components/Map.tsx
@@ -0,0 +1,41 @@
+// components/Map.tsx
+"use client";
+
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+// Fix for default marker icons
+const DefaultIcon = L.icon({
+  iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
+// Coordinates
+const STANFORD_COORDS: [number, number] = [37.4275, -122.1697];
+const ENGINEERING_CENTER_COORDS: [number, number] = [37.4299, -122.1753];
+
+export default function Map() {
+  return (
+    <MapContainer
+      center={STANFORD_COORDS}
+      zoom={15}
+      style={{ height: '100%', width: '100%' }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={ENGINEERING_CENTER_COORDS}>
+        <Popup>Jen-Hsun Huang Engineering Center</Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
